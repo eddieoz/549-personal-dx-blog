6792bdf27046f1c9f1d484c4f84607187f3fe327
Edilson Osorio Jr <osoriojr@pm.me>
2020-05-14 22:37:56 +0300

    update .gitignore

---
+++ networks-sample.js
@@ -0,0 +1,22 @@
+const { projectId, mnemonic } = require('./secrets.json');
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+
+module.exports = { 
+  networks: {
+    development: {
+      protocol: 'http',
+      host: 'localhost',
+      port: 8545,
+      gas: 5000000,
+      gasPrice: 5e9,
+      networkId: '*',
+    },
+    goerli: {
+      provider: () => new HDWalletProvider(
+        mnemonic, `https://goerli.infura.io/v3/<insert yout Infura API key>`
+      ),
+      networkId: 5,
+      gasPrice: 10e9    
+    }
+  },
+};
